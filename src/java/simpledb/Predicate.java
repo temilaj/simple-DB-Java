85485a24ef1ef477f8d11aa03c9aaef87625cb0c
unknown <26441234+R-Suresh@users.noreply.github.com>
2020-10-12 23:12:09 -0400

    Added ex 2,3

--- src/java/simpledb/Predicate.java
+++ src/java/simpledb/Predicate.java
@@ -8,6 +8,10 @@ import java.io.Serializable;
 public class Predicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private int field;
+    private Op op;
+    private Field operand;
 
     /** Constants used for return codes in Field.compare */
     public enum Op implements Serializable {
@@ -56,6 +60,9 @@ public class Predicate implements Serializable {
      */
     public Predicate(int field, Op op, Field operand) {
         // some code goes here
+    	this.field = field;
+    	this.op = op;
+    	this.operand = operand;
     }
 
     /**
@@ -64,7 +71,7 @@ public class Predicate implements Serializable {
     public int getField()
     {
         // some code goes here
-        return -1;
+        return field;
     }
 
     /**
@@ -73,7 +80,7 @@ public class Predicate implements Serializable {
     public Op getOp()
     {
         // some code goes here
-        return null;
+        return op;
     }
     
     /**
@@ -82,7 +89,7 @@ public class Predicate implements Serializable {
     public Field getOperand()
     {
         // some code goes here
-        return null;
+        return operand;
     }
     
     /**
@@ -97,6 +104,8 @@ public class Predicate implements Serializable {
      */
     public boolean filter(Tuple t) {
         // some code goes here
+    	if(t.getField(field).compare(op, operand))
+    	return true;
         return false;
     }
 
@@ -106,6 +115,6 @@ public class Predicate implements Serializable {
      */
     public String toString() {
         // some code goes here
-        return "";
+        return "field_id = "+field+" op = "+op+" operand = "+operand;
     }
 }
