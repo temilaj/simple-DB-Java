658a0d0a5a0f7dd6280deeffe43c8763c62ea7d1
unknown <26441234+R-Suresh@users.noreply.github.com>
2020-10-16 21:41:11 -0400

    Passing 5/6

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -197,8 +197,7 @@ public class BufferPool {
         int tableId = t.getRecordId().getPageId().getTableId();
         try
         {
-            HeapFile heapFile = (HeapFile)Database.getCatalog().getDatabaseFile(tableId);
-            ArrayList<Page> updatedPages = heapFile.deleteTuple(tid, t);
+        	ArrayList<Page> updatedPages = Database.getCatalog().getDatabaseFile(tableId).deleteTuple(tid, t);
             for (Page page : updatedPages)
             {
                 PageId pageId = page.getId();
@@ -209,6 +208,7 @@ public class BufferPool {
         }
         catch (Exception e)
         {
+        	e.printStackTrace();
             throw new IOException("Error deleting tuple from table " + tableId);
         }
     }
@@ -248,17 +248,18 @@ public class BufferPool {
             Page page = this.page_hash.get(pid);
             if (page.isDirty() != null)
             {
-                HeapFile heapFile = (HeapFile)Database.getCatalog().getDatabaseFile(pid.getTableId());
-                // write page to disk.
-                heapFile.writePage(page);
+//                HeapFile heapFile = (HeapFile)Database.getCatalog().getDatabaseFile(pid.getTableId());
+//              // write page to disk.
+//              heapFile.writePage(page);
+            	Database.getCatalog().getDatabaseFile(pid.getTableId()).writePage(page);
                 // mark page as not dirty
                 page.markDirty(false, null);
             }
         }
-        else
-        {
-            throw new IOException();
-        }
+//        else
+//        {
+//            throw new IOException();
+//        }
     }
 
     /** Write all pages of the specified transaction to disk.
